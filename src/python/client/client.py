f0921d8c9908a0d76b0fcb8fd5ef7a0763744b7e
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2021-11-12 12:49:42 +0100

    added an option to pass data to the client

--- src/python/client/client.py
+++ src/python/client/client.py
@@ -5,24 +5,25 @@ import sys
 
 class Client:
     def __init__(self, argv: list):
-        # python3 client.py [PORT_NUMBER] [IP_VERSION_NUMBER]
-        if len(argv) < 3 or int(argv[2]) != 6:
+        # python3 client.py [DATA] [PORT_NUMBER] [IP_VERSION_NUMBER]
+        if len(argv) < 4 or int(argv[3]) != 6:
             self.ip_version = 4
             self.host = '127.0.0.1'
         else:
             self.ip_version = 6
             self.host = '::1'
         self.buffer_size = 1024
-        if len(argv) < 2:
+        if len(argv) < 3:
             self.port = 8000
         else:
-            self.port = int(argv[1])
+            self.port = int(argv[2])
+        self.data = 'Hello, world!\n' if len(argv) < 2 else argv[1]
         print("Will send data to ", self.host, ":", self.port)
 
     def send(self):
         binary_stream = io.BytesIO()
         with socket.socket(socket.AF_INET if self.ip_version == 4 else socket.AF_INET6, socket.SOCK_DGRAM) as new_socket:
-            binary_stream.write("Hello, world!\n".encode('ascii'))
+            binary_stream.write(self.data.encode('ascii'))
             binary_stream.seek(0)
             new_socket.sendto(binary_stream.read(), (self.host, self.port))
             print('Send data to server')
