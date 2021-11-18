45e4d51b8f265da0afb4445862f348a8c3a80452
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2021-11-18 23:29:08 +0100

    added support for a message extending buffer size

--- src/python/lib/Socket.py
+++ src/python/lib/Socket.py
@@ -1,6 +1,14 @@
 import socket
 import io
+from numpy import byte
 
+# Datagram structure:
+# D - data, S - size, F - flags, N - number of datagram
+# SSSSSSSS
+# SSSSSSSS
+# FNNNNNNN
+# DDDDDDDD
+# DDDDD...
 class Socket:
     def __init__(self, host: str, port: str):
         self.socket: socket.socket = None
@@ -9,21 +17,58 @@ class Socket:
         self.buffer_size = 32
     
     def read(self) -> None:
-        return self.socket.recvfrom(self.buffer_size)
+        size, current_size = 1, 0
+        data_map: dict = {}
+        while current_size < size:
+            datagram, address = self.socket.recvfrom(self.buffer_size)
+            data, size, _, number = self.__split_read_data(datagram)
+            data_map[number] = data
+            current_size += 1
+        data = b''.join(val for (_, val) in data_map.items())
+        return data, address
+    
+    def __split_read_data(self, datagram):
+        header = datagram[:3]
+        size = int(header[0] * (2 ** 8) + header[1])
+        is_complex = bool(header[2] << 7)
+        number = int(header[2] << 1)
+        return datagram[3:], size, is_complex, number
 
     def send(self, binary_stream: io.BytesIO, address: str = None) -> None:
         datagram_number = 0
         if address is None:
             address = (self.host, self.port)
-        data = self.__split_data(binary_stream.read())
+        data = self.__split_send_data(binary_stream.read())
         for datagram in data:
             self.socket.sendto(datagram, address)
             print('Sending datagram #', datagram_number, ": ", datagram)    # TODO this needs to go
             datagram_number += 1
     
-    def __split_data(self, raw_data: bytes) -> str:
-        data = [ raw_data[i:i+self.buffer_size] for i in range(0, len(raw_data) - self.buffer_size, self.buffer_size) ]
-        data.append(raw_data[-(len(raw_data) % self.buffer_size):])
+    def __split_send_data(self, raw_data: bytes) -> str:
+        if self.buffer_size >= 65536:
+            raise Exception("Buffer size is to big")                        # TODO specify exception type
+        max_size = self.buffer_size - 3
+        data = []
+        for i in range(0, len(raw_data) - max_size, max_size):
+            datagram: bytearray = bytearray(b'')
+            if len(raw_data) // self.buffer_size <= 256:
+                datagram.extend(byte(0))
+            datagram.extend(byte(len(raw_data) // max_size + (1 if len(raw_data) % max_size != 0 else 0)))  # TODO this should be two bytes, but is one, so it will break for bigger messages
+            datagram.extend(byte(0x80 | i))
+            datagram.extend(raw_data[i:i+max_size])
+            data.append(bytes(datagram))
+        datagram: bytearray = bytearray(b'')
+        if len(raw_data) // self.buffer_size <= 256:
+            datagram.extend(byte(0x00))
+        datagram.extend(byte(len(raw_data) // max_size + (1 if len(raw_data) % max_size != 0 else 0)))  # TODO this should be two bytes, but is one, so it will break for bigger messages
+        if len(data) == 0:
+            datagram.extend(byte(0x7F | len(data)))
+        else:
+            datagram.extend(byte(0x80 | len(data)))
+        datagram.extend(raw_data[-(len(raw_data) % max_size):])
+        data.append(bytes(datagram))
+        if len(data) >= 128:
+            raise Exception("Too many packets")                             # TODO specify exception type
         return data
     
     def connect(self) -> None:
