ff2563ea29a0eaeb08cb3deae1cc1baa655bd224
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2021-11-22 18:33:34 +0100

    removed redundant methods

--- src/python/lib/ServerSocketInterface.py
+++ src/python/lib/ServerSocketInterface.py
@@ -2,12 +2,5 @@
 from lib.SocketInterface import SocketInterface
 
 class ServerSocketInterface(SocketInterface):
-    def __init__(self, socket):
-        super().__init__(socket)
-        self.socket = socket
-    
-    def bind(self) -> None:
-        self.socket.bind()
-    
     def read(self) -> tuple:
         return super().read(True)
